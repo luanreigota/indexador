d8b003698fd9b8876bfe9be2ac7ae58534b7295c
luan <luan@192.168.0.22>
2016-10-25 09:49:34 -0200

    nova funcionalidade: agora é possível selecionar a pasta que deseja escanear.

--- indexador/src/main/java/lucene/TesteMain.java
+++ indexador/src/main/java/lucene/TesteMain.java
@@ -1,15 +1,23 @@
 package lucene;
 
+import java.awt.Component;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import javax.swing.JFileChooser;
 
 public class TesteMain {
 
 	
 	public static void main(String[] args) {
 //		Path path = Paths.get(System.getProperty("user.home") + "/bkp-portal-cmrj");
-		Path path = Paths.get(System.getProperty("user.home") + "/index-teste/pastaParaIndexar");
+		JFileChooser chooserDiretorio = new JFileChooser();
+		chooserDiretorio.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); 
+		chooserDiretorio.showOpenDialog(null);
+		String pasta = chooserDiretorio.getSelectedFile().getAbsolutePath();
+//		Path path = Paths.get(System.getProperty("user.home") + "/index-teste/pastaParaIndexar");
+		Path path = Paths.get(pasta);
 		String diretorioDosIndices = System.getProperty("user.home")+"/index-teste/indices";
 		long inicio = System.currentTimeMillis();
 		try {
